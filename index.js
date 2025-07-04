318878df93c1d2338b4c2950b399a9626c15f3e0
William HS G <88966818+hswg94@users.noreply.github.com>
2025-07-04 15:49:31 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 import express from 'express';
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 
 app.get('/health', (req, res) => {
   res.status(200).send('Health Check OK');
