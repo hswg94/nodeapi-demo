3a6c67a215985101c1a2d8218d688253b5662c90
William Gan <hswg94@gmail.com>
2025-07-07 10:01:34 +0800

    upgraded endpoints with logging

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 
 import express from 'express';
 const app = express();
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 3000;
 
 app.get('/health', (req, res) => {
   res.status(200).send('Health Check OK');
+  console.log('Health Check Endpoint Hit by IP:', req.ip);
 });
 
 app.get('/', (req, res) => {
   res.status(200).send('OK');
+  console.log('Root Endpoint Hit by IP:', req.ip);
 });
 
 app.listen(port, () => {
